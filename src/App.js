10a2f88bc88e550ae67a6f17ce5562545f4c931d
Andrew Wallace <andrew.wallace.english@gmail.com>
2021-04-09 15:39:56 +0900

    displays park icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState }from "react";
-import './App.css';
-import ReactMapGL from "react-map-gl"
+import ReactMapGL, {Marker} from "react-map-gl"
+import * as parkData from "./data/skateboard-parks.json";
+import './index.css';
 
 function App() {
   const [viewport, setViewport] = useState({
@@ -12,8 +13,26 @@ function App() {
   })
   return (
     <div className="App">
-      <ReactMapGL{...viewport} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}>
-        markers here
+      <ReactMapGL
+      {...viewport} 
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      mapStyle="mapbox://styles/wallace-a/ckn9vqev925hp17pg7zu5ea1j"
+      onViewportChange={viewport =>{
+        setViewport(viewport);
+      }}
+      >
+        {/* iterate over park data */}
+        {parkData.features.map((park) => (
+          <Marker 
+          key={park.attributes.PARK_ID}
+          longitude={park.geometry.x}
+          latitude={park.geometry.y}
+          >
+            <button class="marker-btn">
+              <img src="/skateboarding.svg" alt="Skatepark Icon"/>
+            </button>
+          </Marker>
+        ))}
       </ReactMapGL>
     </div>
   );
